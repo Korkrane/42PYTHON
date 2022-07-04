af17f94a71b82312dfc171be714b59cd759ad60e
Korkrane <baudoin.haas@gmail.com>
2022-07-04 17:43:35 +0200

    FIX: pycodestyle formatting regex string

--- d00/ex07/filterwords.py
+++ d00/ex07/filterwords.py
@@ -6,7 +6,7 @@ try:
     if len(sys.argv) != 3:
         print("ERROR")
         exit()
-    reg = "\w{" + str(limit + 1) + ",}"
+    reg = r"\w{" + str(limit + 1) + ",}"
     res = re.findall(reg, sys.argv[1])
     print(res)
 except ValueError:
