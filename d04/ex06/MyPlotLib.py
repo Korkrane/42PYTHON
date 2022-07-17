facf16b20ac446f94eb49fa6ad360893fe187e36
Korkrane <baudoin.haas@gmail.com>
2022-07-17 17:55:08 +0200

    ADD: d04 ex06

--- d04/ex06/MyPlotLib.py
+++ d04/ex06/MyPlotLib.py
@@ -4,38 +4,55 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from FileLoader import FileLoader
 
-
 class MyPlotLib:
     def __init__(self):
         pass
 
-    def histogram(self, data, **features):
+    def histogram(self, data, features):
         try:
-            data.plot.hist(bins=10)
+            if len(features) == 1:
+                y = data[features[0]].tolist()
+                plt.hist(y, 15, density=False, facecolor='b', alpha=0.75)
+                plt.title(features[0])
+                plt.grid(True)
+            else:
+                axis = plt.subplots(1, len(features), figsize=(10, 5))[1]
+                for i, elem in enumerate(features):
+                    y = data[elem].tolist()
+                    axis[i].hist(y, 15, density=False, facecolor='b', alpha=0.75)
+                    axis[i].set_title(elem)
+                    axis[i].grid(True)
             plt.show()
-            return
         except Exception as e:
-            print("Error :", e)
+            print(e)
 
-    def density(self, data, **features):
+    def density(self, data, features):
         try:
-            return
+            axis = plt.subplots(figsize=(10, 5))[1]
+            for elem in features:
+                pd.DataFrame(data[elem]).plot(ax=axis, kind='density')
+            plt.show()
         except Exception as e:
-            print("Error :", e)
+            print(e)
 
-    def pair_plot(self, data, **features):
+    def pair_plot(self, data, features):
         try:
-            return
+            sns.pairplot(data.filter(items=features), diag_kind = 'hist', diag_kws = {'bins':10})
+            plt.show()
         except Exception as e:
-            print("Error :", e)
+            print(e)
 
-    def box_plot(self, data, **features):
+    def box_plot(self, data, features):
         try:
-            return
+            data.boxplot(column=features, grid=False, figsize=(10, 5))
+            plt.show()
         except Exception as e:
-            print("Error :", e)
+            print(e)
 
 loader = FileLoader()
 data = loader.load("../athlete_events.csv")
 mpl = MyPlotLib()
-mpl.histogram(data, bins=10)
+mpl.histogram(data, ['Height', 'Weight'])
+# mpl.density(data, ['Weight', 'Height'])
+mpl.pair_plot(data, ['Weight', 'Height'])
+# mpl.box_plot(data, ['Weight', 'Height'])
